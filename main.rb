869e88bc0bbe4cfdd8dd021385b296b9becca95e
Christian <christianvillere@gmail.com>
2021-08-27 22:12:38 -0700

    Finish move_player_x function

--- main.rb
+++ main.rb
@@ -21,17 +21,31 @@ class Board
   :game_board
 
   def initialize first_row, second_row, third_row, game_board
-    @first_row = []
-    @second_row = []
-    @third_row = []
+    @first_row = [' ', ' ', ' ']
+    @second_row = [' ', ' ', ' ']
+    @third_row = [' ', ' ', ' ']
     @game_board = [@first_row,
                    @second_row,
                    @third_row]
   end
-  def display_board game_board
+
+  def display_board(game_board)
     p game_board
   end
 
-  def move_player_x game_board
+  def move_player_x(game_board)
+    puts 'Player 1: Where would you like to put "x"?'
+    puts 'Row?'
+    puts 'Column?'
+    row = gets.to_i
+    column = gets.to_i
+    row -= 1
+    column -= 1
+    play = [row, column]
+    if @game_board[play[0]][play[1]] != ' ' 
+      puts 'Chose again! Spot already taken!'
+    else
+      @game_board[play[0]][play[1]] = ' x '
   end
+
 end

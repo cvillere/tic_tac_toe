0be33abb677eae7755f5a9ee13577a9d9e1851b2
Christian <christianvillere@gmail.com>
2021-09-11 08:34:05 -0700

    Break win functions into smaller func.

--- main.rb
+++ main.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 require 'pry'
+
 # creates GameBoard class to initiate a tic-tac-toe board
 class Board
-
-
   def initialize
     @gameboard = []
   end
 
   def add_gameboard_rows
     i = 0
-    while i < 3 do
+    while i < 3
       @gameboard.push(['[ ]', '[ ]', '[ ]'])
       i += 1
     end
@@ -17,42 +18,38 @@ class Board
 
   def display_board
     # puts played_board.class
-    @gameboard = @gameboard.each{| x |
+    @gameboard = @gameboard.each do |x|
       puts x.join('')
-    }
-    # @gameboard
+    end
   end
 
   def cats_game(game)
     cats_result = []
-    game.each { | x |
+    game.each do |x|
       if x.include?('[ ]')
-        cats_result.push("play")
+        cats_result.push('play')
       else
-        cats_result.push("cats")
+        cats_result.push('cats')
       end
-      }
-    if cats_result.all?("cats")
-      puts "Cat's Game!"
-      return "Cat's Game!"
     end
+    puts "Cat's Game!" if cats_result.all?('cats')
   end
 
-  def continue_game gameboard
+  def continue_game(gameboard)
     nested_array_result = []
-    gameboard.each {|x|
-      if x.include?("[ ]")
-        nested_array_result.push("play on!")
+    gameboard.each do |x|
+      if x.include?('[ ]')
+        nested_array_result.push('play on!')
       else
         nested_array_result.push("cat's game!")
       end
-    }
-    return nested_array_result
+    end
+    nested_array_result
   end
 end
 
+# Class that does game play moves
 class PlayGame < Board
-
   def position_x_check(move)
     if @gameboard[move[0]][move[1]] != '[ ]'
       puts 'Chose again! Spot already taken!'
@@ -96,84 +93,93 @@ class PlayGame < Board
   end
 end
 
+# Class that executes game play moves
 class GamePlay < PlayGame
-
   def add_x
-    display_board()
-    position_x()
+    display_board
+    position_x
   end
 
   def add_o
-    display_board()
-    position_o()
+    display_board
+    position_o
   end
 
   def add_x_o
-    while continue_game(@gameboard).all?("cat's game!") == false do
-      add_x()
+    while continue_game(@gameboard).all?("cat's game!") == false
+      add_x
       check_winner(@gameboard)
       if continue_game(@gameboard).all?("cat's game!") == true
-        display_board()
+        display_board
         return cats_game(@gameboard)
       end
 
-      add_o()
+      add_o
       check_winner(@gameboard)
       if continue_game(@gameboard).all?("cat's game!") == true
-        display_board()
+        display_board
         return cats_game(@gameboard)
       end
     end
   end
 
   def row_win(game)
-    for i in 0..2 do
+    (0..2).each do |i|
       if game[i][0] == ' x ' && game[i][1] == ' x ' && game[i][2] == ' x '
-        display_board()
+        display_board
         puts 'player_X won the game!!'
         exit
       end
-      if game[i][0] == ' o ' && game[i][1] == ' o ' && game[i][2] == ' o '
-        display_board()
-        puts 'player_O won the game!!'
-        exit
-      end
+      next unless game[i][0] == ' o ' && game[i][1] == ' o ' && game[i][2] == ' o '
+
+      display_board
+      puts 'player_O won the game!!'
+      exit
     end
   end
 
   def column_win(game)
-    for i in 0..2 do
+    (0..2).each do |i|
       if game[0][i] == ' x ' && game[1][i] == ' x ' && game[2][i] == ' x '
-        display_board()
+        display_board
         puts 'player_X won the game!!'
         exit
       end
-      if game[0][i] == ' o ' && game[1][i] == ' o ' && game[2][i] == ' o '
-        display_board()
-        puts 'player_O won the game!!'
-        exit
-      end
-    end
-  end
-  
-  def cross_win(game)
-    if game[0][0] == ' x ' && game[1][1] == ' x ' && game[2][2] == ' x '
-      display_board()
-      puts 'player_X won the game!!'
-      exit
-    end 
-    if game[0][0] == ' o ' && game[1][1] == ' o ' && game[2][2] == ' o '
-      display_board()
+      next unless game[0][i] == ' o ' && game[1][i] == ' o ' && game[2][i] == ' o '
+
+      display_board
       puts 'player_O won the game!!'
       exit
     end
-    if game[2][0] == ' x ' && game[1][1] == ' x ' && game[0][2] == ' x '
-      display_board()
+  end
+
+  def cross_win_x_left(game)
+    if game[0][0] == ' x ' && game[1][1] == ' x ' && game[2][2] == ' x '
+      display_board
       puts 'player_X won the game!!'
       exit
     end
+  end
+
+  def cross_win_x_right(game)
+    return unless game[2][0] == ' x ' && game[1][1] == ' x ' && game[0][2] == ' x '
+      display_board
+      puts 'player_X won the game!!'
+      exit
+    end
+  end
+
+  def cross_win_o_left(game)
+    return unless game[0][0] == ' o ' && game[1][1] == ' o ' && game[2][2] == ' o '
+      display_board
+      puts 'player_O won the game!!'
+      exit
+    end
+  end
+
+  def cross_win_o_right(game)
     if game[2][0] == ' o ' && game[1][1] == ' o ' && game[0][2] == ' o '
-      display_board()
+      display_board
       puts 'player_O won the game!!'
       exit
     end
@@ -182,16 +188,12 @@ class GamePlay < PlayGame
   def check_winner(game)
     row_win(game)
     column_win(game)
-    cross_win(game)
+    cross_win_x_left(game)
+    cross_win_x_right(game)
+    cross_win_o_left(game)
+    cross_win_o_right(game)
   end
-
 end
-
-=begin
-new_board = Board.new()
-new_board.add_gameboard_rows
-new_board.display_board
-=end
 
 second_board = GamePlay.new
 second_board.add_gameboard_rows
